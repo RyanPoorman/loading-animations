13a4bf94f591f72beec34a5d8a54eed10e0efb19
RyanPoorman <ryanpoorman@192.168.1.2>
2018-03-11 18:51:03 -0500

    Major changes to the Grid.js file to begin the enabling dynamic base settings

--- Grid.js
+++ Grid.js
@@ -1,42 +1,34 @@
-var column = 0;
-var row = 1;
+var row = 0.0;
+var gridSize = 200;
+var base = 5;
+var denominator = 20; //2-25 to adjust the speed of the overall animation
 
 function generateLoading() {
-	
-	var base = 2;
-	var numOfSquares = base*base; //1,4,9,16,25,36...
-	
-
-	
-	for(i = 1; i <= numOfSquares; i++) {
+	var numOfSquares = base * base; // 1,4,9,16,25,36...
+	changeContainerSize();
+	for (i = 0; i < numOfSquares; i++) {
 		createSquare(base, i);
-		
 	}
 }
 
+function changeContainerSize() {
+	var containers = document.getElementsByClassName("square-grid");
+	containers[0].style.width = gridSize + "px";
+	containers[0].style.height = gridSize + "px";
+}
 
 function createSquare(base, i) {
 	var square = document.createElement("DIV");
-	square.style.height = 100/base+"%";
-	square.style.width = 100/base+"%";
+	square.style.height = 100 / base + "%";
+	square.style.width = 100 / base + "%";
 	square.style.animationDelay = getAnimationTime(i, base);
 	square.classList.add("square");
-	
-
 	document.getElementById("dynamicGrid").append(square);
 }
 
-
 function getAnimationTime(index, base) {
-	
-	
-	if((index / base) % 1 == 0) {
-		console.log(index);
-		row++;
-	}
-	
-
-	
-	return "0."+ Math.trunc(index) +"s"
+	var currentRow = Math.floor(index / base);
+	index = (index - (currentRow * base)) + currentRow;
+	return (index / denominator) + "s";
 }
 
